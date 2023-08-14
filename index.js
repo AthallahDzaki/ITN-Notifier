d0508bc6d4da3d60a499ee6d38994be59cf13cf7
AthallahDzaki <AthallahDzaki@gmail.com>
2023-08-15 00:06:06 +0700

    feat: Carousel & Base64Video

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+require("dotenv").config();
 const axios = require("axios");
+const imageToBase64 = require('image-to-base64');
 const fs = require("fs");
-const { downloadFile } = require("./file_dl");
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 
-const WHURL = "https://discord.com/api/webhooks/1140227257009455194/jG_lQDnwiaoIcENoZ-gpseuB1vqcu3a9J05E04Z2aRUkME3pQYzxBcjLkjcn-e5TRYu4";
+const WHURL = process.env.WebhookURL; // Main
+
+
+let g_Cookies = "";
 
 async function GetData(username)
 {
     return new Promise(async (resolve, reject) => {
+        if(!g_Cookies)
+        {
+            throw new Error("Please Login First");
+        }
         axios.get("https://www.instagram.com/api/v1/users/web_profile_info/?username="+username, {
             headers: {
-                'cookie': 'ig_did=A7B9BE90-3987-4708-9EC5-01C9024672F6; mid=YuqQsAALAAF-BBhrsTkWdFQmtfmi; datr=14xiY9FgvZkcdAldxv-NH52b; fbm_124024574287414=base_domain=.instagram.com; ig_nrcb=1; dpr=2; csrftoken=5DoB49kAhQFikosFgqwneC0qzzfDTN8A; ds_user_id=54482988145; sessionid=54482988145%3A5ZwTHcLfywDSAR%3A8%3AAYdXkwfRZyDVJloK3JrERlbcBwsvl3aHjlpM6bOHwA; rur="PRN\/05454482988145\/0541723535375:01f75142dc67a95df8624478d07844cd30ed953635e9a7bcf32cbf9f6d602b738f14c680"',
+                'cookie': g_Cookies,
                 'user-agent': "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36",
                 'X-Ig-App-Id': "1217981644879628",
                 ["sec-fetch-site"]: "same-origin"
@@ -37,11 +45,14 @@ async function GetData(username)
 
                 const carouselNodes = el?.['node']?.['edge_sidecar_to_children']?.['edges'];
                 if (carouselNodes?.length) {
-                    obj.carousel = [...carouselNodes]?.map(async (node) => {
-                        let temp = { imageUrl: node?.['node']?.['display_url'] }
-                        if (base64imagesCarousel) temp.image = await imageToBase64(node?.['node']?.['display_url'])
-                        return temp;
-                    });
+                    let carouselData = [];
+                    for(let i = 0; i < carouselNodes.length; i++)
+                    {
+                        let obj = { imageUrl: carouselNodes[i]?.['node']?.['display_url'] }
+                        if(true) obj.image = await imageToBase64(carouselNodes[i]?.['node']?.['display_url']);
+                        carouselData.push(obj);
+                    }
+                    obj.carousel = carouselData;
                 }
 
                 if (image) obj.image = image
@@ -52,7 +63,7 @@ async function GetData(username)
                 ) {
                     obj.videoUrl = el?.['node']?.['video_url'];
                     obj.videoViewCount = el?.['node']?.['video_view_count'];
-                    if (false) obj.video = await imageToBase64(el?.['node']?.['video_url']);
+                    if (true) obj.video = await imageToBase64(el?.['node']?.['video_url']);
                 }
 
                 return obj
@@ -75,10 +86,34 @@ async function LoginToRealDevice()
     4. Get Header Cookies from /timeline/ path
     5. Return Cookies as plain text and used at GetData Function
     */
+    let browser = await puppeteer.launch({
+        headless: false,
+    })
+    let page = await browser.newPage();
+    await page.goto("https://www.instagram.com/");
+    await page.waitForSelector('input[name=username]');
+    //await page.$eval('input[name=username]', el => el.value = 'rekayasa.perangkat.lembek');
+    await page.type('input[name=username]', process.env.IG_USERNAME);
+    await page.type('input[name=password]', process.env.IG_PASSWORD);
+    await page.click('button[type=submit]');
+    await page.waitForNavigation();
+    if(await page.url() == "https://www.instagram.com/accounts/onetap/?next=%2F")
+    {
+        await page.click("div[role=button]");
+        await page.waitForNavigation();
+    }
+    let cookie = await page.cookies("https://www.instagram.com/timeline/");
+    let cookieStr = "";
+    for(let i = 0; i < cookie.length; i++)
+    {
+        cookieStr += `${cookie[i].name}=${cookie[i].value.replaceAll("\\", "\/")}; `;
+    }
+    await browser.close();
+    return cookieStr;
 }
 
 (async () => {
-    // LoginToRealDevice();
+    g_Cookies = await LoginToRealDevice();
     await GetFTIContent().then(async (res) => {
         console.log("FTI : ", res);
     }).catch((err) => {
@@ -157,11 +192,19 @@ async function GetFTIContent()
                 ]
             };
     
-            if(typeof(ret[3]?.imageUrl) != "undefined")
+            if(typeof(ret[3]?.imageUrl) != "undefined" && typeof(ret[3]?.carousel) == "undefined")
             {
                 post.embeds[0].image = {
                     "url": `${ret[3].imageUrl}`
                 };
+            }
+            else if(typeof(ret[3]?.carousel) != "undefined")
+            {
+                post.embeds[0].fields.push({
+                    "name": "Carousel",
+                    "value": `Carousel is Under This Message`,
+                    "inline": true
+                });
             }
     
             axios.post(WHURL, post).catch((err) => {
@@ -170,39 +213,54 @@ async function GetFTIContent()
     
             if(typeof(ret[3]?.videoUrl) != "undefined")
             {
-                await downloadFile(ret[3].videoUrl, ret[3].id+".mp4").then(() => {
-
-                    const FormData = require('form-data');
-                    const form = new FormData;
-        
-                    form.append('username', "FTI - Institut Teknologi Nasional Malang");
-        
-                    form.append('avatar_url', "https://instagram.fsub8-2.fna.fbcdn.net/v/t51.2885-19/365146966_1260129631148068_5489059564946239368_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fsub8-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=pbDnmiruy9UAX9_omyG&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfD6zmUKOrVcRPQfeXMjLgmytEapQZ2sur4Rmri0b-hInA&oe=64DD8599&_nc_sid=8b3546");
-        
-                    form.append('file', fs.createReadStream(ret[3].id+".mp4"));
-                    
-                    form.submit(WHURL, (error, response) => {
-                        
-                        if (error) throw new Error(error);
-                        if (response) 
-                        {
-                            console.log("Uploaded MP4 Because Have a Video");
-                            fs.unlink(ret[3].id+".mp4", (err) => {
-                                if (err) {
-                                    reject(err);
-                                }
-                                fs.writeSync(fs.openSync("latest_id_FTI.txt", "w"), ret[3].id);
-                                resolve("ALL Process Done");
-                            })
-                        }
-                    });
-                }).catch((err) => {
-                    reject("Error When Downloading Video");
-                });
+                const buffer = Buffer.from(ret[3]?.video, 'base64');
+                const FormData = require('form-data');
+                const form = new FormData;
     
+                form.append('username', "FTI - Institut Teknologi Nasional Malang");
+    
+                form.append('avatar_url', "https://instagram.fsub8-2.fna.fbcdn.net/v/t51.2885-19/365146966_1260129631148068_5489059564946239368_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fsub8-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=pbDnmiruy9UAX9_omyG&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfD6zmUKOrVcRPQfeXMjLgmytEapQZ2sur4Rmri0b-hInA&oe=64DD8599&_nc_sid=8b3546");
+    
+                form.append('upload', buffer, ret[3].id+".mp4");
+                
+                form.submit(WHURL, (error, response) => {
+                    
+                    if (error) reject(error);
+                    if (response) 
+                    {
+                        console.log("Uploaded MP4 Because Have a Video");
+                        fs.writeSync(fs.openSync("latest_id_FTI.txt", "w"), ret[3].id);
+                        resolve("ALL Process Done");
+                    }
+                });
             }
             else
             {
+                if(typeof(ret[3]?.carousel) != "undefined")
+                {
+                    for(let i = 0; i < ret[3]?.carousel.length; i++)
+                    {
+                        console.log(ret[3]?.carousel[i].image);
+                        const buffer = Buffer.from(ret[3]?.carousel[i].image, 'base64');
+                        const FormData = require('form-data');
+                        const form = new FormData;
+
+                        form.append('username', "ITN Malang News");
+
+                        form.append('avatar_url', "https://scontent-xsp1-1.cdninstagram.com/v/t51.2885-19/56649586_593617321153064_8169530956705693696_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-xsp1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=Rxdzav8QwOsAX8knDqS&edm=AOQ1c0wBAAAA&ccb=7-5&cb_e2o_trans=t&oh=00_AfCK5sOIN9f1A4yp76Xhck4ThU1r_m9yPHhdvCgp8dpMMA&oe=64DCD642&_nc_sid=8b3546");
+
+                        form.append('upload', buffer, ret[3].id +"_"+ i +".jpg");
+                        
+                        form.submit(WHURL, (error, response) => {
+                            
+                            if (error) reject(error);
+                            if (response) 
+                            {
+                                console.log("Uploaded Carousel "+i+" Because Have a Carousel Length");
+                            }
+                        });
+                    }
+                }
                 fs.writeSync(fs.openSync("latest_id_FTI.txt", "w"), ret[3].id);
                 resolve("ALL Process Done");
             }
@@ -281,20 +339,31 @@ async function GetINewsContent()
                 ]
             };
 
-            if(typeof(ret[3]?.imageUrl) != "undefined")
+            if(typeof(ret[3]?.imageUrl) != "undefined" && typeof(ret[3]?.carousel) == "undefined")
             {
                 post.embeds[0].image = {
                     "url": `${ret[3].imageUrl}`
                 };
+            }
+            else if(typeof(ret[3]?.carousel) != "undefined")
+            {
+                post.embeds[0].fields.push({
+                    "name": "Carousel",
+                    "value": `Carousel is Under This Message`,
+                    "inline": true
+                });
             }
 
             axios.post(WHURL, post).catch((err) => {
                 reject(err);
             });
 
-            if(typeof(ret[3]?.videoUrl) != "undefined")
+            if(typeof(ret[3]?.carousel) != "undefined")
             {
-                await downloadFile(ret[3].videoUrl, ret[3].id+".mp4").then(() => {
+                for(let i = 0; i < ret[3]?.carousel.length; i++)
+                {
+                    console.log(ret[3]?.carousel[i].image);
+                    const buffer = Buffer.from(ret[3]?.carousel[i].image, 'base64');
                     const FormData = require('form-data');
                     const form = new FormData;
 
@@ -302,29 +371,69 @@ async function GetINewsContent()
 
                     form.append('avatar_url', "https://scontent-xsp1-1.cdninstagram.com/v/t51.2885-19/56649586_593617321153064_8169530956705693696_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-xsp1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=Rxdzav8QwOsAX8knDqS&edm=AOQ1c0wBAAAA&ccb=7-5&cb_e2o_trans=t&oh=00_AfCK5sOIN9f1A4yp76Xhck4ThU1r_m9yPHhdvCgp8dpMMA&oe=64DCD642&_nc_sid=8b3546");
 
-                    form.append('file', fs.createReadStream(ret[3].id+".mp4"));
+                    form.append('upload', buffer, ret[3].id +"_"+ i +".jpg");
                     
                     form.submit(WHURL, (error, response) => {
                         
                         if (error) reject(error);
                         if (response) 
                         {
-                            console.log("Uploaded MP4 Because Have a Video");
-                            fs.unlink(ret[3].id+".mp4", (err) => {
-                                if (err) {
-                                    reject(err);
-                                }
-                                fs.writeSync(fs.openSync("latest_id_INEWS.txt", "w"), ret[3].id);
-                                resolve("ALL Process Done");
-                            })
+                            console.log("Uploaded Carousel "+i+" Because Have a Carousel Length");
                         }
                     });
-                }).catch((err) => {
-                    reject("Error When Downloading Video");
+                }
+            }
+
+            if(typeof(ret[3]?.videoUrl) != "undefined")
+            {
+                const buffer = Buffer.from(ret[3]?.video, 'base64');
+                const FormData = require('form-data');
+                const form = new FormData;
+
+                form.append('username', "ITN Malang News");
+
+                form.append('avatar_url', "https://scontent-xsp1-1.cdninstagram.com/v/t51.2885-19/56649586_593617321153064_8169530956705693696_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-xsp1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=Rxdzav8QwOsAX8knDqS&edm=AOQ1c0wBAAAA&ccb=7-5&cb_e2o_trans=t&oh=00_AfCK5sOIN9f1A4yp76Xhck4ThU1r_m9yPHhdvCgp8dpMMA&oe=64DCD642&_nc_sid=8b3546");
+
+                form.append('upload', buffer, ret[3].id+".mp4");
+                
+                form.submit(WHURL, (error, response) => {
+                    
+                    if (error) reject(error);
+                    if (response) 
+                    {
+                        console.log("Uploaded MP4 Because Have a Video");
+                        fs.writeSync(fs.openSync("latest_id_INEWS.txt", "w"), ret[3].id);
+                        resolve("ALL Process Done");
+                    }
                 });
             }
             else
             {
+                if(typeof(ret[3]?.carousel) != "undefined")
+                {
+                    for(let i = 0; i < ret[3]?.carousel.length; i++)
+                    {
+                        console.log(ret[3]?.carousel[i].image);
+                        const buffer = Buffer.from(ret[3]?.carousel[i].image, 'base64');
+                        const FormData = require('form-data');
+                        const form = new FormData;
+
+                        form.append('username', "ITN Malang News");
+
+                        form.append('avatar_url', "https://scontent-xsp1-1.cdninstagram.com/v/t51.2885-19/56649586_593617321153064_8169530956705693696_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-xsp1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=Rxdzav8QwOsAX8knDqS&edm=AOQ1c0wBAAAA&ccb=7-5&cb_e2o_trans=t&oh=00_AfCK5sOIN9f1A4yp76Xhck4ThU1r_m9yPHhdvCgp8dpMMA&oe=64DCD642&_nc_sid=8b3546");
+
+                        form.append('upload', buffer, ret[3].id +"_"+ i +".jpg");
+                        
+                        form.submit(WHURL, (error, response) => {
+                            
+                            if (error) reject(error);
+                            if (response) 
+                            {
+                                console.log("Uploaded Carousel "+i+" Because Have a Carousel Length");
+                            }
+                        });
+                    }
+                }
                 fs.writeSync(fs.openSync("latest_id_INEWS.txt", "w"), ret[3].id);
                 resolve("ALL Process Done");
             }
